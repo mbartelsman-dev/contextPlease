70c45a8d23a4a37e1965881caac42960ff8f3b2a
Miguel Bartelsman <mbartelsmanm@gmail.com>
2021-04-22 00:35:25 +0200

    Initial commit

---
+++ ContextPlease2/Configuration.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+
+namespace ContextPlease2
+{
+    /// <summary>
+    /// Data class containing the configuration for ContextPlease
+    /// </summary>
+    public class Configuration
+    {
+        /// <summary>
+        /// Path to the input JSON configuration file
+        /// </summary>
+        public string InputPath { get; }
+        
+        /// <summary>
+        /// Path to the base output file name. Has no extension
+        /// </summary>
+        public string BaseOutputPath { get; }
+
+        /// <summary>
+        /// Should pre-existing registry keys be kept when writing new ones?
+        /// </summary>
+        public bool KeepOld { get; }
+        
+        /// <summary>
+        /// Should an extra registry script for the removal of the described menu be created?
+        /// </summary>
+        public bool MakeUninstaller { get; }
+        
+        /// <summary>
+        /// Path to the output installer file.
+        /// </summary>
+        public string InstallerPath => Path.Join(BaseOutputPath, ".install.reg");
+        
+        /// <summary>
+        /// Path to the output uninstaller file.
+        /// </summary>
+        public string UninstallerPath => Path.Join(BaseOutputPath, ".remove.reg");
+
+        /// <summary>
+        /// Builds a new Settings object
+        /// </summary>
+        /// <param name="inputPath">Path to input file</param>
+        /// <param name="baseOutputPath">Path to output file</param>
+        /// <param name="keepOld">Should pre-existing registry keys be kept when writing new ones?</param>
+        /// <param name="makeUninstaller">Should a removal registry script be generated?</param>
+        public Configuration(
+            string inputPath,
+            string? baseOutputPath,
+            bool keepOld,
+            bool makeUninstaller)
+        {
+
+            baseOutputPath ??= Path.GetFileNameWithoutExtension(inputPath);
+
+            if (Path.EndsInDirectorySeparator(baseOutputPath))
+            {
+                baseOutputPath = Path.Combine(baseOutputPath, Path.GetFileNameWithoutExtension(inputPath));
+            }
+
+            if (Path.GetExtension(baseOutputPath) == ".reg")
+            {
+                baseOutputPath = Path.GetFileNameWithoutExtension(baseOutputPath);
+            }
+            
+            InputPath = inputPath;
+            BaseOutputPath = baseOutputPath;
+            KeepOld = keepOld;
+            MakeUninstaller = makeUninstaller;
+        }
+    }
+}
---
+++ ContextPlease2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace ContextPlease2
+{
+    class Program
+    {
+        
+        /// <summary>
+        /// Entry point function
+        /// </summary>
+        /// <param name="input">Path to the input JSON configuration file</param>
+        /// <param name="output">Path to the generated REG files</param>
+        /// <param name="keepOld">Should pre-existing registry keys be kept when writing new ones?</param>
+        /// <param name="makeUninstaller">Should a removal registry script be generated?</param>
+        static void Main(
+            string input,
+            string? output,
+            bool keepOld = false,
+            bool makeUninstaller = true)
+        {
+            try
+            {
+                Configuration configuration = ReadArguments(input, output, keepOld, makeUninstaller);
+                
+                ContextEntryModel menuModel = new(configuration);
+                menuModel.ReadInput();
+                
+                var registryHandle = menuModel.RegistryHandle;
+                registryHandle.WriteInstaller(configuration.InstallerPath);
+                
+                if (configuration.MakeUninstaller)
+                {
+                    registryHandle.WriteRemover(configuration.UninstallerPath);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Environment.Exit(-1);
+            }
+        }
+
+        /// <summary>
+        /// Validates and normalizes command line arguments and assembles a settings file.
+        /// </summary>
+        /// <param name="input">Path to input file</param>
+        /// <param name="output">Path to output file</param>
+        /// <param name="keepOld">Should pre-existing registry keys be kept when writing new ones?</param>
+        /// <param name="makeUninstaller">Should a removal registry script be generated?</param>
+        /// <returns>Settings object with the program settings</returns>
+        /// <exception cref="ArgumentNullException">If input is null</exception>
+        /// <exception cref="FileNotFoundException">If input file cannot be found</exception>
+        static Configuration ReadArguments(
+            string input,
+            string? output,
+            bool keepOld,
+            bool makeUninstaller)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(
+                    paramName: nameof(input),
+                    message: "An input file must be specified");
+            }
+
+            if (!File.Exists(input))
+            {
+                throw new FileNotFoundException(
+                    fileName: input,
+                    message: $"Input file could not be found: {input}");
+            }
+
+            return new Configuration(input, output, keepOld, makeUninstaller);
+        }
+    }
+}
